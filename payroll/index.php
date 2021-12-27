b92e2643e5533be6b20674e512e6f847177149de
Suraj Kumar Verma <63317279+surajverma8787@users.noreply.github.com>
2021-12-27 22:57:06 +0530

    Add files via upload

---
+++ payroll/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+       <head>
+	        <meta charset="utf-8">
+	        <title>Welcome to homepage</title>
+			<link rel="stylesheet" href="style.css">
+	   </head>
+	   <body>
+	   
+	        <div class="loginbox">
+			    <h3>Sign In</h3><br>
+			    <form method="post" action="newfile.php">
+				     <p>Username</p><br>
+					 <input type="text" name="username" placeholder="admin">
+					 <p>Password</p><br>
+					 <input type="password" name="password" placeholder="*****">
+					 <input type="submit" name="submit" value="Log In">
+				</form>
+			</div>
+	   </body>
+</html>
